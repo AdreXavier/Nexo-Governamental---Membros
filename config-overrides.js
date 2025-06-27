e4c7ef6fa9a9e9b4d7adb770e308cdf7cb7d9f0a
AdreXavier <165933295+AdreXavier@users.noreply.github.com>
2025-06-26 21:37:54 -0300

    Delete config-overrides.js

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const { override, addBabelPlugin } = require('customize-cra');
-
-module.exports = override(
-  // Adiciona o plugin Babel para suportar decoradores
-  // 'legacy: true' é importante para a compatibilidade com a sintaxe atual de decoradores
-  addBabelPlugin(['@babel/plugin-proposal-decorators', { legacy: true }])
-);
